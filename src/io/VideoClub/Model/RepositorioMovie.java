c03f1706c3d1c323371b480e004c941490cd1ac5
ANDREA <ANDREA@LAPTOP-RJ4L81SJ>
2020-03-26 13:16:30 +0100

    REPOSITORIOS TERMINADOS SIN XML AÚN

--- src/io/VideoClub/Model/RepositorioMovie.java
+++ src/io/VideoClub/Model/RepositorioMovie.java
@@ -5,10 +5,50 @@
  */
 package io.VideoClub.Model;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
 /**
  *
  * @author ANDREA
  */
-public class RepositorioMovie {
+public class RepositorioMovie extends HashSet<Movie> {
+    private static RepositorioMovie instance;
+    private RepositorioMovie(){};
     
+    public static RepositorioMovie getInstance(){
+        if(instance==null){
+            instance=new RepositorioMovie();
+        }
+        return instance;
+    
+    }
+
+    @Override
+    public String toString() {
+        String result= "-----REPOSITORIO-----\n";
+        for(Movie m:instance){
+            result+="> "+m+"\n";
+                    
+        }
+        return result;
+    }
+    
+    public List<Movie> filter(String regularEspression){
+        List<Movie> movies=new ArrayList<>();
+        return movies;
+        
+    }
+    
+    public void save(){
+        List<Movie> l=new ArrayList<>();
+        l.addAll(this);
+        //XMLManager.writeList_into_XML(l, "games.XML");
+    }
+    
+    public boolean load(){
+        this.clear();
+        return this.addAll(/*XMLManager.readXML_into_List(games.XML)*/);
+    }
 }
--- src/io/VideoClub/Model/RepositorioProduct.java
+++ src/io/VideoClub/Model/RepositorioProduct.java
@@ -5,10 +5,50 @@
  */
 package io.VideoClub.Model;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+
 /**
  *
  * @author ANDREA
  */
-public class RepositorioProduct {
+public class RepositorioProduct extends HashSet<Product> {
+    private static RepositorioProduct instance;
+    private RepositorioProduct(){};
     
+    public static RepositorioProduct getInstance(){
+        if(instance==null){
+            instance=new RepositorioProduct();
+        }
+        return instance;
+    
+    }
+
+    @Override
+    public String toString() {
+        String result= "-----REPOSITORIO-----\n";
+        for(Product p:instance){
+            result+="> "+p+"\n";
+                    
+        }
+        return result;
+    }
+    
+    public List<Product> filter(String regularEspression){
+        List<Product> products=new ArrayList<>();
+        return products;
+        
+    }
+    
+    public void save(){
+        List<Product> l=new ArrayList<>();
+        l.addAll(this);
+        //XMLManager.writeList_into_XML(l, "games.XML");
+    }
+    
+    public boolean load(){
+        this.clear();
+        return this.addAll(/*XMLManager.readXML_into_List(games.XML)*/);
+    }
 }
